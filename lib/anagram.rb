baa68ad94459a502a344cbb14e7a9d10244335f1
atencioluke <atencioluke@gmail.com>
2020-08-12 22:05:33 -0700

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :name
+    def initialize(word)
+        @name = word
+    end
+
+    def match(arr)
+        arr.map { |word| word if word.chars.sort == @name.chars.sort }.compact
+    end
+
+end
